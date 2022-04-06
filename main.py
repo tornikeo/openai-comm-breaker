61d3b50436f0da9685a55d4ca472c035bea856c8
tornikeo <tornikeonoprishvili@gmail.com>
2022-04-06 20:50:16 +0400

    Add notebook

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from dotenv import dotenv_values
-
+import os
+import openai
 config = dotenv_values(".env")
-print(config)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt="Correct this to standard English:\n\nShe no went to the market.",
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+print(response)
